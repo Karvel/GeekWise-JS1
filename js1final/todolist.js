440293d385c0a878d3c8e462100313674c9847a6
karvel <jadeilyn@yahoo.com>
2015-12-06 16:14:00 -0800

    Initial commit of todo list final project. Basic shell javascript and html, no added visual styling yet.

---
+++ js1final/todolist.js
@@ -0,0 +1,18 @@
+/*
+Elanna Grossman
+December 3, 2015
+GeekWise Javascript I
+Final Project
+
+Simple Todo List
+*/
+
+function getItem()
+{
+    var todoItem = document.getElementById("addItem").value;
+    document.getElementById("addItem").value = "";
+
+    var list = document.getElementById("listContain");
+
+    list.insertAdjacentHTML('afterbegin', '<li>' + todoItem + '</li>');
+}//end getItem
