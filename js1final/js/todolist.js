5c80c00a495e11dffea98ccd422fcbcc3548f5ee
karvel <jadeilyn@yahoo.com>
2015-12-07 23:56:20 -0800

    Created array structure for todo list items, cleaned up code.

--- js1final/js/todolist.js
+++ js1final/js/todolist.js
@@ -7,12 +7,24 @@ Final Project
 Simple Todo List
 */
 
+var todoArray = [];
+
+function todoAdd(todoItem)
+{
+    //var todoListOutput += todoList[i].push;
+    todoArray.push(todoItem);
+    return todoArray;
+}//end todoAdd
+
 function getItem()
 {
+    var top = todoArray.length;
     var todoItem = document.getElementById("addItem").value;
+    todoAdd(todoItem);
     document.getElementById("addItem").value = "";
 
     var list = document.getElementById("listContain");
 
-    list.insertAdjacentHTML('afterbegin', '<li>' + todoItem + '</li>');
+    list.insertAdjacentHTML('afterbegin', '<li>' + todoArray[top] + '</li>');
+    top += 1;
 }//end getItem
